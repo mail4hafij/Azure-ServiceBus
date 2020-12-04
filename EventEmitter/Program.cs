abbec22a3516d7036f9cd05a5ad71935128395b8
Rahman Mohammad Hafijur <mohammad-hafijur.rahman@bostad.uppsala.se>
2020-12-05 00:18:40 +0100

    A simple demonstration of Azure-ServiceBus

---
+++ EventEmitter/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventEmitter
+{
+    class Program
+    {
+        private const string ServiceBusConnectionString = "topic_send_key_connection_string";
+        private const string TopicName = "topic_name";
+        
+        static async Task Main()
+        {
+            ITopicClient topicClient = new TopicClient(ServiceBusConnectionString, TopicName);
+
+            var data = new SomeData()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Data = "Hello world!"
+            };
+            var payload = JsonConvert.SerializeObject(data);
+
+            var msg = new Message(Encoding.UTF8.GetBytes(payload));
+            msg.UserProperties.Add("Type", "eventData");
+            msg.MessageId = Guid.NewGuid().ToString();
+
+            await topicClient.SendAsync(msg);
+            await topicClient.CloseAsync();
+            Console.WriteLine("Event emitted successfully.");
+        }
+
+        public class SomeData
+        {
+            public string Id { get; set; }
+            public string Data { get; set; }
+        }
+
+    }
+}
---
+++ EventSubscriber/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventSubscriber
+{
+    class Program
+    {
+        private const string ServiceBusConnectionString = "topic_read_key_connection_string";
+        private const string TopicName = "topic_name";
+        private const string SubscriptionName = "subcription_name";
+        private static ISubscriptionClient _subscriptionClient;
+        
+        static async Task Main()
+        {
+            _subscriptionClient = new SubscriptionClient(ServiceBusConnectionString, TopicName, SubscriptionName);
+
+            // ExceptionHandler
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler);
+            // ProcessMessageHandler
+            _subscriptionClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+
+            await _subscriptionClient.CloseAsync();
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            Console.WriteLine($"Event received: {Encoding.UTF8.GetString(message.Body)}");
+            await _subscriptionClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine(exceptionReceivedEventArgs.Exception);
+            return Task.CompletedTask;
+        }
+    }
+}
